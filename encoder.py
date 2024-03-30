33155165cfc3d5c39c0760bbee7a14aefafc6f12
em-1001 <80628552+em-1001@users.noreply.github.com>
2024-03-31 00:33:49 +0900

    Add files via upload

---
+++ encoder.py
@@ -0,0 +1,97 @@
+# VAE encoder
+
+import torch
+from torch import nn
+from torch.nn import functional as F
+from decoder import VAE_AttentionBlock, VAE_ResidualBlock
+
+class VAE_Encoder(nn.Sequential):
+    def __init__(self):
+        super().__init__(
+            # (Batch_Size, 3, Height, Width) -> (Batch_Size, 128, Height, Width)
+            nn.Conv2d(3, 128, kernel_size=3, padding=1),
+
+            # (Batch_Size, 128, Height, Width) -> (Batch_Size, 128, Height, Width)
+            VAE_ResidualBlock(128, 128),
+
+            # (Batch_Size, 128, Height, Width) -> (Batch_Size, 128, Height, Width)
+            VAE_ResidualBlock(128, 128),
+
+            # (Batch_Size, 128, Height, Width) -> (Batch_Size, 128, Height / 2 , Width / 2)
+            nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=0),
+
+            # (Batch_Size, 128, Height / 2 , Width / 2) -> (Batch_Size, 256, Height / 2 , Width / 2)
+            VAE_ResidualBlock(128, 256),
+
+            # (Batch_Size, 256, Height / 2 , Width / 2) -> (Batch_Size, 256, Height / 2 , Width / 2)
+            VAE_ResidualBlock(256, 256),
+
+            # (Batch_Size, 256, Height / 2 , Width / 2) -> (Batch_Size, 256, Height / 4 , Width / 4)
+            nn.Conv2d(256, 256, kernel_size=3, stride=2, padding=0),
+
+            # (Batch_Size, 256, Height / 4 , Width / 4) -> (Batch_Size, 512, Height / 4 , Width / 4)
+            VAE_ResidualBlock(256, 512),
+
+            # (Batch_Size, 512, Height / 4 , Width / 4) -> (Batch_Size, 512, Height / 4 , Width / 4)
+            VAE_ResidualBlock(512, 512),
+
+            # (Batch_Size, 512, Height / 4 , Width / 4) -> (Batch_Size, 512, Height / 8 , Width / 8)
+            nn.Conv2d(512, 512, kernel_size=3, stride=2, padding=0),
+
+            VAE_ResidualBlock(512, 512),
+
+            VAE_ResidualBlock(512, 512),
+
+            # (Batch_Size, 512, Height / 8 , Width / 8) -> (Batch_Size, 512, Height / 8 , Width / 8)
+            VAE_ResidualBlock(512, 512),
+
+            # (Batch_Size, 512, Height / 8 , Width / 8) -> (Batch_Size, 512, Height / 8 , Width / 8)
+            VAE_AttentionBlock(512),
+
+            # (Batch_Size, 512, Height / 8 , Width / 8) -> (Batch_Size, 512, Height / 8 , Width / 8)
+            VAE_ResidualBlock(512, 512),
+
+            # (Batch_Size, 512, Height / 8 , Width / 8) -> (Batch_Size, 512, Height / 8 , Width / 8)
+            nn.GroupNorm(32, 512),
+
+            # (Batch_Size, 512, Height / 8 , Width / 8) -> (Batch_Size, 512, Height / 8 , Width / 8)
+            nn.SiLU(),
+
+            # (Batch_Size, 512, Height / 8 , Width / 8) -> (Batch_Size, 8, Height / 8 , Width / 8)
+            nn.Conv2d(512, 8, kernel_size=3, padding=1),
+
+            # (Batch_Size, 8, Height / 8 , Width / 8) -> (Batch_Size, 8, Height / 8 , Width / 8)
+            nn.Conv2d(8, 8, kernel_size=1, padding=0)
+        )
+
+    def forward(self, x: torch.Tensor, noise: torch,Tensor) -> torch.Tensor:
+        # x: (Batch_Size, Channel, Height, Width)
+        # noise: (Batch_Size, Out_Channel, Height / 8, Width / 8)
+
+        for module in self:
+            if getattr(module, 'stride', None) == (2, 2):
+
+                # (Padding_Left, Padding_Right, Padding_Top, Padding_Bottom)
+                x = F.pad(x, (0, 1, 0, 1)) # padding을 right side와 buttom side에만 주고 싶을 때
+            x = module(x)
+
+        # (Batch_Size, 8, Height / 8, Width / 8) -> two tensors of shape (Batch_Size, 4, Height / 8, Width / 8)
+        mean, log_variance = torch.chunk(x, 2, dim=1)
+
+        # (Batch_Size, 4, Height / 8, Width / 8) -> (Batch_Size, 4, Height / 8, Width / 8)
+        log_variance = torch.clamp(log_variance, -30, 20)
+
+        # (Batch_Size, 4, Height / 8, Width / 8) -> (Batch_Size, 4, Height / 8, Width / 8)
+        variance = log_variance.exp()
+
+        # (Batch_Size, 4, Height / 8, Width / 8) -> (Batch_Size, 4, Height / 8, Width / 8)
+        stdev = variance.sqrt()
+
+        # Z = N(0, 1) -> N(mean, variance)?
+        # x = mean + stdev * Z -> Reparameterize Trick
+        x = mean + stdev * noise
+
+        # Scale the output by a constant
+        x *= 0.18215
+
+        return x
---
+++ pipeline.py
@@ -0,0 +1,150 @@
+# pipline.py
+
+import torch
+import numpy as np
+from tqdm import tqdm
+from ddpm import DDPMSampler
+
+WIDTH = 512
+HEIGHT = 512
+LATENTS_WIDTH = WIDTH // 8
+LATENTS_HEIGHT = HEIGHT // 8
+
+def generate(prompt: str, uncond_prompt: str, input_image=None, strength=0.0,
+             do_cfg=True, cfg_scale=7.5, sampler_name="ddpm", n_inference_steps=50,
+             models={}, seed=None, device=None, idle_device=None, tokenizer=None):
+
+    with torch.no_grad():
+        if not (0 < strength <= 1):
+            raise ValueError("strength must be in [0, 1]")
+
+        if idle_device:
+            to_idle = lambda x: x.to(idle_device)
+        else:
+            to_idle = lambda x: x
+
+        generator = torch.Generator(device=device)
+        if seed is None:
+            generate.seed()
+        else:
+            generator.manual_seed(seed)
+
+        clip = models["clip"]
+        clip.to(device)
+
+        if do_cfg:
+            # Convert the prompt into tokens using the tokenizer
+            cond_tokens = tokenizer.batch_encode_plus([prompt], padding="max_length", max_length=77).input_ids
+            # (Batch_Size, Seq_Len)
+            cond_tokens = torch.tensor(cond_tokens, dtype=torch.long, device=device)
+            # (Batch_Size, Seq_Len) -> (Batch_Size, Seq_Len, Dim)
+            cond_context = clip(cond_tokens)
+
+            uncond_tokens = tokenizer.batch_encode_plus([uncond_prompt], padding="max_length", max_length=77).input_ids
+            uncond_tokens = torch.tensor(uncond_tokens, dtype=torch.long, device=device)
+            # (Batch_Size, Seq_Len) -> (Batch_Size, Seq_Len, Dim)
+            uncond_context =clip(uncond_tokens)
+
+            # (2, Seq_Len, Dim) -> (2, 77, 768)
+            context = torch.cat([cond_context, uncond_context])
+        else:
+            # Convert it into a list of tokens
+            tokens = tokenizer.batch_encode_plus([prompt], padding="max_length", max_length=77).input_ids
+            tokens = torch.tensor(tokens, dtype=torch.long, device=device)
+            # (1, 77, 768)
+            context = clip(tokens)
+        to_idle(clip)
+
+        if sampler_name == "ddpm":
+            sampler = DDPMSampler(generator)
+            sampler.set_inference_timesteps(n_inference_steps)
+
+        else:
+            raise ValueError(f"Unknown sampler {sampler_name}")
+
+        latents_shape = (1, 4, LATENTS_HEIGHT, LATENTS_WIDTH)
+
+        if input_image:
+            encoder = models["encoder"]
+            encoder.to(device)
+
+            input_image_tensor = input_image.resize((WIDTH, HEIGHT))
+            input_image_tensor = np.array(input_image_tensor)
+            # (Height, Width, Channel)
+            input_image_tensor = torch.tensor(input_image_tensor, dtype=torch.float32, device=device)
+            # rescale (0 ~ 255) -> (-1 ~ 1)
+            input_image_tensor = rescale(input_image_tensor, (0, 255), (-1, 1))
+            # (Height, Width, Channel) -> (Batch_Size, Height, Width, Channel)
+            input_image_tensor = input_image_tensor.unsqueeze(0)
+            # (Batch_Size, Height, Width, Channel) -> (Batch_Size, Channel, Height, Width)
+            input_image_tensor = input_image_tensor.permute(0, 3, 1, 2)
+
+            encoder_noise = torch.randn(latents_shape, generator=generator, device=device)
+            # run the image through the encoder of the VAE
+            latents = encoder(input_image_tensor, encoder_noise)
+
+            sampler.set_strength(strength=strength)
+            latents = sampler.add_noise(latents, sampler.timestep[0])
+
+            to_idle(encoder)
+
+        else:
+            # If we are doing text-to-image, start with random noise N(0, I)
+            latents = torch.randn(latents_shape, generator=generator, device=device)
+
+        diffusion = models["diffusion"]
+        diffusion.to(device)
+
+        timesteps = tqdm(sampler.timesteps)
+        for i , timestep in enumerate(timesteps):
+            # (1, 320)
+            time_embedding = get_time_embedding(timestep).to(device)
+
+            # (Batch_Size, 4, Latents_Height, Latents_Width)
+            model_input = latents
+
+            if do_cfg:
+                # (Batch_Size, 4, Latent_Height, Latent_Width) -> (2 * Batch_Size, 4, Latent_Height, Latent_Width)
+                model_input = model_input.repeat(2, 1, 1, 1)
+
+            # model_output is the prediected noise by the UNET
+            model_output = diffusion(model_input, context, time_embedding)
+
+            if do_cfg:
+                output_cond, output_uncond = model_output.chunk(2)
+                model_output = cfg_scale * (output_cond - output_uncond) + output_uncond
+
+            # Remove noise predicted by the UNET
+            latents = sampler.step(timestep, latents, model_output)
+
+        to_idle(diffusion)
+
+        decoder = models['decoder']
+        decoder.to(device)
+
+        images = decoder(latents)
+        to_idle(decoder)
+
+        images = rescale(images, (-1, 1), (0, 255), clamp=True)
+        # (Batch_Size, Channel, Height, Width) -> (Batch_Size, Height, Width, Channel)
+        images = images.permute(0, 2, 3, 1)
+        images = images.to("cpu", torch.uint8).numpy()
+        return images[0]
+
+def rescale(x, old_range, new_range, clamp=False):
+    old_min, old_max = old_range
+    new_min, new_max = new_range
+    x -= old_min
+    x *= (new_max - new_min) / (old_max - old_min)
+    x + new_min
+    if clamp:
+        x = x.clamp(new_min, new_max)
+    return x
+
+def get_time_embedding(timestep):
+    # (160, )
+    freqs = torch.pow(10000, -torch.arange(start=0, end=160, dtype=torch.float32) / 160)
+    # (1, 160)
+    x = torch.tensor([timestep], dtype=torch.float32)[:, None] * freqs[None]
+    # (1, 320)
+    return torch.cat([torch.cos(x), torch.sin(x)], dim=-1)
